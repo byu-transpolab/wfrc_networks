8892d6a5707b69f74472657c682e26d81b257df0
Greg Macfarlane <gregmacfarlane@byu.edu>
2021-03-12 15:50:19 -0700

    Transform segments layer into wgs84

--- R/segments_maker.R
+++ R/segments_maker.R
@@ -5,7 +5,10 @@ library(foreign)
 #read the WFRC shapefile and convert to geojson to stash the outputs
 #in the repo 
 segs_with_factors <- st_read("/Users/gregmacfarlane/Box/Macfarlane/research/strategic_transit/Utah Network/WFRC Network Stuff/TravelDemandModel/TDM_master_segments/Master_Segs_withFactors_20200504.shp")
-st_write(segs_with_factors, "data/segs_with_factors.geojson", )
+
+segs_with_factors %>%
+  st_transform(4326) %>%
+  st_write("data/segs_with_factors.geojson", delete_dsn = TRUE)
 
 #read the output summary from the TDM
 segment_summaries <- read.dbf("/Users/gregmacfarlane/Box/Macfarlane/research/strategic_transit/Utah Network/WFRC Network Stuff/TravelDemandModel/TDM_segment_summaries/v831_SE19_Net19_Summary_SEGID_Detailed.dbf")
